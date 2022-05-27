7a5618c0ca06fdf56b8bd2a01c56ef4778610838
Jaehwan Kim <jaehwkim@c3r6s1.42seoul.kr>
2022-05-27 11:16:03 +0900

    feat.make main page

--- lv1_4/forth_js.js
+++ lv1_4/forth_js.js
@@ -1,27 +1,41 @@
-function move1(){
-	location.href ='/lv1_4/video_page.html';
-}
-function move2(){
-	location.href ='/lv1_4/video_page.html';
-}
-function move3(){
-	location.href ='/lv1_4/video_page.html';
-}
-function move4(){
-	location.href ='/lv1_4/video_page.html';
-}
-function move5(){
-	location.href ='/lv1_4/video_page.html';
-}
-function move6(){
-	location.href ='/lv1_4/video_page.html';
-}
-function move7(){
-	location.href ='/lv1_4/video_page.html';
-}
 function move(){
-	location.href ='/lv1_4/video_page.html';
+	location.href ='/lv1_4/video_page';
 }
-function move2(){
-	location.href ='/lv1_4/video_page.html';
+
+{/* <div class="image">
+			<img class="imge__img" src="../lalaland/1.another-day-of-sun.jpg" alt="AnotherDayOfSun">
+			<div class="image__overlay image__overlay--blur">
+				<div class="image__title"><h1><a onclick="move1();" style="cursor:pointer">play</a></h1></div>
+			</div>
+			<div class="name">
+				<h1><a onclick="swal('The name is', 'Another Day Of Sun.', 'success');" style="cursor:pointer">Another Day Of Sun</a></h1>
+			</div> */}
+
+
+
+
+let movieName =[
+	'1.another-day-of-sun',
+	'2.some-one-in-the-crowd',
+	'3.mia-and-sebastian_s-theme',
+	'4.I-Ran',
+	'5.Herman_s-Habit',
+	'6.Summer-Montage',
+	'7.Engagement-Party',
+	'8.Audition',
+	'9.Epilogue'
+
+];
+
+let wrapper = document.querySelector(".wrapper");
+
+function divMaker (src, alt, ...args) {
+	const div = document.createElement("div");
+	div.className = "image";
+	return div;
 }
+
+wrapper.appendChild(divMaker());
+
+
+
